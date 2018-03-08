75493c4703212367d3ea730db2e89ac813d48ccd
Maxim <yakomak137@gmail.com>
2018-03-08 14:06:01 +0300

    version 3.1

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,26 @@
-const checker = function() {
-  if (document.querySelector('.is-due-now')) {
-    console.log(document.querySelector('.is-due-now').title);
-  }
+var elms_soon = document.querySelectorAll('.is-due-soon');
+var elms_now = document.querySelectorAll('.is-due-now');
+var elms_past = document.querySelectorAll('.is-due-past');
+
+const chek = function(elms, color1, color2) {
+	for (var i = elms.length - 1; i >= 0; i--) {
+		elm = elms[i]; 
+		var k=0;
+		while(k<8) {
+	    if(elm === null) { break; }
+			if(elm.className === 'list js-list-content') {elm.style.background = color1}
+			if(elm.className === 'js-list list-wrapper') {elm.style.background = color2}
+			elm = elm.parentElement; 
+			k++;
+		}
+	}
 }
 
-window.setInterval(checker, 10000);
+
+const chekk = function() {   
+	chek(elms_past, 'brown', 'red');
+  chek(elms_soon, 'yellow', 'red');
+	chek(elms_now, 'red', 'red');
+}
+
+window.setInterval(chekk, 5000);
